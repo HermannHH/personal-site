460fddd55f268869c577f95af5bb85cdf76825e5
Hermann Harris <harris.hermann@gmail.com>
2021-01-21 09:46:09 +0200

    WIP

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   if (document.readyState === "interactive" || document.readyState === "complete" ) {
-    new Typewriter('#typewriter', {
-      strings: ['Hello', 'World'],
-      autoStart: true,
+    var space = document.getElementById('typewriter');
+    var typewriter = new Typewriter(space, {
+      loop: false
     });
+    typewriter
+    .typeString('<h1 class="h1 bold">Hello.</h1>')
+    .pauseFor(1500)
+    .typeString('<h1 class="h1 bold">My name is Hermann</h1>')
+    .pauseFor(1500)
+    .typeString('<p class="p">My name is Hermann</p>')
+    .start();
   }
 });
